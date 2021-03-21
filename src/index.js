e0d0077018c8ca4116bfaad334ef3706d294d614
Julio Muller <juliodolago@gmail.com>
2021-03-21 16:26:37 -0300

    Created action to create schemas in PostgreSQL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import colors from 'colors'
 import pgBenchmark from './benchmarks/postgres'
 import mongoBenchmark from './benchmarks/mongo'
+import { loadDataset } from './database/postgres'
 
 function printStart(msg) {
   console.log(' ', colors.cyan(msg))
@@ -11,6 +12,10 @@ function printEnd(msg) {
 }
 
 async function main() {
+  printStart('Criando esquemas no PostgreSQL e carregando dados...')
+  await loadDataset()
+  printEnd('PostgreSQL carregado!')
+
   printStart('Iniciando benchmark do PostgreSQL...')
   await pgBenchmark.run()
   printEnd('Benchmark do PostgreSQL finalizado!')
