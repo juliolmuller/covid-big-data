f5bb785448870570e64a77fff4ef579e5d5b6879
Julio Muller <juliodolago@gmail.com>
2021-03-21 03:53:15 -0300

    Added support to connect to Mongo with user & password

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -6,7 +6,10 @@ dotenv.config()
 const host = process.env.DB_MONGO_HOST
 const port = process.env.DB_MONGO_PORT
 const database = process.env.DB_MONGO_DATABASE
-const url = `${host}:${port}/${database}`
+const username = process.env.DB_MONGO_USERNAME
+const password = process.env.DB_MONGO_PASSWORD
+const credentials = username ? `${username}:${password}@` : ''
+const url = `mongodb://${credentials}${host}:${port}/${database}`
 
 export async function getConnection() {
   const client = new MongoClient(url)
