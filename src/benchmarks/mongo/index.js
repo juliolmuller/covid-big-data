e2f6085bfcf91f4fabe11a3e8012de7c3f33a021
Julio Muller <juliodolago@gmail.com>
2021-03-23 05:34:35 -0300

    Implemented benchmarks to MongoDB

--- src/benchmarks/mongo/index.js
+++ src/benchmarks/mongo/index.js
@@ -1,14 +1,68 @@
+import colors from 'colors'
 import { getConnection } from '../../database/mongo'
+import * as queries from './queries'
+
+async function displayBenchmark(description, queryPromise) {
+  const DESCRIPTION_LENGTH = 60
+  const COUNT_LENGTH = 17
+  const DURATION_LENGTH = 8
+
+  try {
+    const { docs, duration } = await queryPromise
+
+    console.log('   ',
+      description.padEnd(DESCRIPTION_LENGTH),
+      `${docs.length} registro${docs.length === 1 ? ' ' : 's'}`.padStart(COUNT_LENGTH),
+      `${duration}ms`.padStart(DURATION_LENGTH))
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 async function runBenchmarks() {
   const client = await getConnection()
+  const collection = client.db().collection(process.env.DB_MONGO_COLL1)
 
   try {
-    const dbs = await client.db().admin().listDatabases()
-
-    console.log(dbs)
+    await displayBenchmark('Buscar lista de cidades e estados', queries.getAllPlaces(collection))
+    await displayBenchmark('Buscar lista de estados', queries.getAllStates(collection))
+    await displayBenchmark('Buscar lista de cidades do Acre', queries.getAllCities(collection, 'AC'))
+    await displayBenchmark('Buscar lista de cidades do Amzonas', queries.getAllCities(collection, 'AM'))
+    await displayBenchmark('Buscar lista de cidades da Bahia', queries.getAllCities(collection, 'BA'))
+    await displayBenchmark('Buscar lista de cidades do Ceará', queries.getAllCities(collection, 'CE'))
+    await displayBenchmark('Buscar lista de cidades do Distrito Federal', queries.getAllCities(collection, 'DF'))
+    await displayBenchmark('Buscar lista de cidades do Mato Grosso', queries.getAllCities(collection, 'MT'))
+    await displayBenchmark('Buscar lista de cidades do Paraná', queries.getAllCities(collection, 'PR'))
+    await displayBenchmark('Buscar lista de cidades do Rio de Janeiro', queries.getAllCities(collection, 'RJ'))
+    await displayBenchmark('Buscar lista de cidades de São Paulo', queries.getAllCities(collection, 'SP'))
+    // await displayBenchmark('Buscar todos os registros', queries.getAll(collection)) // TODO: uncomment
+    await displayBenchmark('Buscar página 1 de tudo com 50 registros', queries.getPage(collection, 1, 50))
+    await displayBenchmark('Buscar página 100 de tudo com 50 registros', queries.getPage(collection, 100, 50))
+    await displayBenchmark('Buscar página 1000 de tudo com 50 registros', queries.getPage(collection, 1000, 50))
+    await displayBenchmark('Buscar página 1000 de tudo com 30 registros', queries.getPage(collection, 1000, 30))
+    await displayBenchmark('Buscar registro de Curitiba em 1/abril/2020', queries.getByDateAndPlace(collection, '2020-04-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar registro de Curitiba em 1/junho/2020', queries.getByDateAndPlace(collection, '2020-06-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar registro de Curitiba em 1/agosto/2020', queries.getByDateAndPlace(collection, '2020-08-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar registro de Curitiba em 1/outubro/2020', queries.getByDateAndPlace(collection, '2020-10-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar registro de Curitiba em 1/dezembro/2020', queries.getByDateAndPlace(collection, '2020-12-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar registro de Curitiba em 1/fevereiro/2021', queries.getByDateAndPlace(collection, '2021-02-01', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar somatório em Curitiba em março/2020', queries.getTotalByDateRangeAndPlace(collection, '2020-03-01', '2020-03-31', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar somatório em Curitiba em maio/2020', queries.getTotalByDateRangeAndPlace(collection, '2020-05-1', '2020-05-31', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar somatório em Curitiba em julho/2020', queries.getTotalByDateRangeAndPlace(collection, '2020-07-1', '2020-07-31', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar somatório em Curitiba em setembro/2020', queries.getTotalByDateRangeAndPlace(collection, '2020-09-1', '2020-09-30', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar somatório em Curitiba e região em novembro/2020', queries.getTotalByDateRangeAndPlace(collection, '2020-11-01', '2020-11-30', { PR: ['Curitiba', 'Araucária', 'Colombo', 'Campo Largo', 'Fazenda Rio Grande', 'Pinhais', 'São José dos Pinhais'] }))
+    await displayBenchmark('Buscar somatório em Curitiba e região em janeiro/2021', queries.getTotalByDateRangeAndPlace(collection, '2021-01-01', '2021-01-31', { PR: ['Curitiba', 'Araucária', 'Colombo', 'Campo Largo', 'Fazenda Rio Grande', 'Pinhais', 'São José dos Pinhais'] }))
+    await displayBenchmark('Buscar média em Curitiba em março/2020', queries.getAverageByDateRangeAndPlace(collection, '2020-03-01', '2020-03-31', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar média em Curitiba em junho/2020', queries.getAverageByDateRangeAndPlace(collection, '2020-06-01', '2020-06-30', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar média em Curitiba em setembro/2020', queries.getAverageByDateRangeAndPlace(collection, '2020-09-01', '2020-09-30', { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar média em Curitiba e região em setembro/2020', queries.getAverageByDateRangeAndPlace(collection, '2020-09-01', '2020-09-30', { PR: ['Curitiba', 'Araucária', 'Colombo', 'Campo Largo', 'Fazenda Rio Grande', 'Pinhais', 'São José dos Pinhais'] }))
+    await displayBenchmark('Buscar percentual em Curitiba em 1/março/2021', queries.getPercentageByDateRangeAndPlace(collection, { PR: 'Curitiba' }, '2021-03-01'))
+    await displayBenchmark('Buscar percentual atual em todas as cidades', queries.getPercentageByDateRangeAndPlace(collection))
+    await displayBenchmark('Buscar percentual em Curitiba em 19/março/2021', queries.getPercentageByDateRangeAndPlace(collection, { PR: 'Curitiba' }))
+    await displayBenchmark('Buscar percentual em Curitiba e região em 1/março/2021', queries.getPercentageByDateRangeAndPlace(collection, { PR: ['Curitiba', 'Araucária', 'Colombo', 'Campo Largo', 'Fazenda Rio Grande', 'Pinhais', 'São José dos Pinhais'] }, '2021-03-01'))
+    await displayBenchmark('Buscar percentual em Curitiba e região em 19/março/2021', queries.getPercentageByDateRangeAndPlace(collection, { PR: ['Curitiba', 'Araucária', 'Colombo', 'Campo Largo', 'Fazenda Rio Grande', 'Pinhais', 'São José dos Pinhais'] }))
   } catch (error) {
-    console.error(error)
+    throw error
   } finally {
     client.close()
   }
---
+++ src/benchmarks/mongo/queries.js
@@ -0,0 +1,183 @@
+
+// Executa a query e retorna um objeto com os timestamps
+async function queryRunner(query) {
+  const startTimestamp = Date.now()
+  const docs = await query.toArray()
+  const endTimestamp = Date.now()
+  const duration = endTimestamp - startTimestamp
+
+  return {
+    startTimestamp,
+    endTimestamp,
+    duration,
+    docs,
+  }
+}
+
+// Transformar { estado: cidade } em SQL para cláusula WHERE
+function getPlacesClosure(places) {
+  if (!places) {
+    return {}
+  }
+
+  // Se for uma lista estados ['PR', 'SC', ...]
+  if (places instanceof Array) {
+    return {
+      $or: places.map((state) => {
+        return { city: '', state }
+      }),
+    }
+  }
+
+  // Se for um mapa de estado-cidade(s) { estado: cidade(s) }
+  const closures = []
+
+  Object.entries(places).forEach(([state, city]) => {
+    if (city instanceof Array) {
+      const innerClosures = city.map((c) => getPlacesClosure({ [state]: c }).$or[0])
+
+      closures.push(...innerClosures)
+    } else {
+      closures.push({ city, state })
+    }
+  })
+
+  return { $or: closures }
+}
+
+// Recupera a lista de estados e cidades
+export function getAllPlaces(collection) {
+  return queryRunner(collection.aggregate([
+    { $group: { _id: { state: '$state', city: '$city' } } },
+  ]))
+}
+
+// Recupera a lista de estados
+export function getAllStates(collection) {
+  return queryRunner(collection.aggregate([
+    { $group: { _id: { state: '$state' } } },
+  ]))
+}
+
+// Recupera a lista de cidades para o dado estado
+export function getAllCities(collection, state) {
+  return queryRunner(collection.aggregate([
+    { $match: { state } },
+    { $group: { _id: { state: '$state', city: '$city' } } },
+  ]))
+}
+
+// Recupera todos os registros do banco, sem filtros
+export function getAll(collection, fields = {}) {
+  return queryRunner(collection.find({}, fields))
+}
+
+// Recupera parte dos registros do banco, sem filtros, por paginação
+export function getPage(collection, page, limit, fields = {}) {
+  return queryRunner(collection
+    .find({}, fields)
+    .skip(limit * (page - 1))
+    .limit(limit))
+}
+
+// Recupera o(s) registro(s) para uma determinada cidade (ou cidades) em uma determinada data
+export function getByDateAndPlace(collection, date, places, fields = {}) {
+  return queryRunner(collection
+    .find({
+      ...getPlacesClosure(places),
+      date: new Date(date),
+    }, fields))
+}
+
+// Recupera a soma de casos e óbitos para uma determinada cidade (ou cidades) em um determinado período
+export function getTotalByDateRangeAndPlace(collection, startDate, endDate, places) {
+  return queryRunner(collection
+    .aggregate([{
+      $match: {
+        ...getPlacesClosure(places),
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    }, {
+      $group: {
+        _id: {
+          city: '$city',
+          state: '$state',
+          city_ibge_code: '$city_ibge_code',
+          estimated_population_2019: '$estimated_population_2019',
+        },
+        sum_confirmed: { $sum: '$new_confirmed' },
+        sum_deaths: { $sum: '$new_deaths' },
+      },
+    }]))
+}
+
+// Recupera a média de casos e óbitos para uma determinada cidade (ou cidades) em um determinado período
+export function getAverageByDateRangeAndPlace(collection, startDate, endDate, places) {
+  return queryRunner(collection
+    .aggregate([{
+      $match: {
+        ...getPlacesClosure(places),
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    }, {
+      $group: {
+        _id: {
+          city: '$city',
+          state: '$state',
+          city_ibge_code: '$city_ibge_code',
+          estimated_population_2019: '$estimated_population_2019',
+        },
+        avg_confirmed: { $avg: '$new_confirmed' },
+        avg_deaths: { $avg: '$new_deaths' },
+      },
+    }]))
+}
+
+// Recupera a média de casos e óbitos para uma determinada cidade (ou cidades) em um determinado período
+export function getPercentageByDateRangeAndPlace(collection, places, date) {
+  return queryRunner(collection
+    .aggregate([{
+      $addFields: {
+        rate_confirmed: {
+          $function: {
+            body: 'function (cases, population) { return Math.round(cases / population * 1000) / 10 }',
+            args: ['$last_available_confirmed', '$estimated_population_2019'],
+            lang: 'js',
+          },
+        },
+        rate_deaths: {
+          $function: {
+            body: 'function (cases, population) { return Math.round(cases / population * 1000) / 10 }',
+            args: ['$last_available_deaths', '$estimated_population_2019'],
+            lang: 'js',
+          },
+        },
+      },
+    }, {
+      $match: {
+        ...getPlacesClosure(places),
+        ...(() => (date
+          ? { date: new Date(date) }
+          : { is_last: true }))(),
+      },
+    }, {
+      $group: {
+        _id: {
+          city: '$city',
+          state: '$state',
+          city_ibge_code: '$city_ibge_code',
+          estimated_population_2019: '$estimated_population_2019',
+          total_confirmed: '$last_available_confirmed',
+          total_deaths: '$last_available_deaths',
+          rate_confirmed: '$rate_confirmed',
+          rate_deaths: '$rate_deaths',
+        },
+      },
+    }]))
+}
